d0e1012b94ae2b0aa69e0a4b5b3a464b648faf12
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-09-21 18:27:50 +0300

    Use global fetch in Electron renderer with a static check
    The point is that (process.type === 'browser') is compile-time
    optimizable, so it can remove the unused require from the tree.

--- fetch.js
+++ fetch.js
@@ -1,5 +1,16 @@
 'use strict'
 
-// Try to use window.fetch (e.g. if in Electron), otherwise node-fetch
-// Doesn't use non-window global fetch if present as a precaution, but that might change
-module.exports = typeof window !== 'undefined' && window.fetch || require('node-fetch')
+// Try to use global fetch (e.g. if in Electron), otherwise node-fetch
+if (typeof process !== 'undefined' && process) {
+  // Node.js or Electron with Node.js integration
+  if (process.type === 'browser') {
+    // Node.js or Electron browser process
+    module.exports = require('node-fetch')
+  } else {
+    // Electron renderer with Node.js integration
+    module.exports = fetch
+  }
+} else {
+  // Browser or Electron without Node.js integration
+  module.exports = fetch
+}
