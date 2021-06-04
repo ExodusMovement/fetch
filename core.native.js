353269cc6ffc3f0b882c8eee83d13d7db22f8944
Andrew Toth <andrewstoth@gmail.com>
2021-06-04 14:20:05 -0400

    fix: require node-fetch if native fetch is undefined

--- core.native.js
+++ core.native.js
@@ -1,4 +1,5 @@
 'use strict'
 
 // fetch and WebSocket already exist in react-native, we don't want to bundle impls
-module.exports = { fetch, WebSocket }
+// for node tests in mobile though this will throw
+module.exports = { fetch: typeof fetch === 'undefined' ? require('node-fetch') : fetch, WebSocket }
