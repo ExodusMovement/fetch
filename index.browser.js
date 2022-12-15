f4e098b42b97ea52f8b86d5a1dcb1398a52180e9
Nikita Skovoroda <chalkerx@gmail.com>
2022-12-15 23:32:21 +0400

    feat: replace fetchival with a rewritten version

--- index.browser.js
+++ index.browser.js
@@ -3,6 +3,6 @@
 // Don't need to require node-fetch here, global fetch is defined
 // Same for global WebSocket
 
-const fetchival = require('fetchival')
+const fetchival = require('./fetchival.browser.js')
 
 module.exports = { fetch, WebSocket, fetchival }
