575c66ace782a14a18b609d8dc1aabb478d3b66a
Rocco Musolino <roccomuso@users.noreply.github.com>
2025-02-26 10:15:11 +0100

    chore: export wretch (#27)

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 const fetch = require('./fetch')
 const WebSocket = require('./websocket')
 const fetchival = require('./fetchival')
+const wretch = require('./wretch')
 
-module.exports = { fetch, WebSocket, fetchival }
+module.exports = { fetch, WebSocket, fetchival, wretch }
