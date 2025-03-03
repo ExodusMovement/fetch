2f114ab53213dc30295216a03c1d22c3d6b6a51e
Rocco Musolino <roccomuso@users.noreply.github.com>
2025-03-03 10:57:36 +0100

    fix: revert wretch export (#31)

--- index.js
+++ index.js
@@ -5,6 +5,5 @@
 const fetch = require('./fetch')
 const WebSocket = require('./websocket')
 const fetchival = require('./fetchival')
-const wretch = require('./wretch')
 
-module.exports = { fetch, WebSocket, fetchival, wretch }
+module.exports = { fetch, WebSocket, fetchival }
